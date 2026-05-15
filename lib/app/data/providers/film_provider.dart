918ef7efa0b6c67c9dfbfe92b2e9be88d9ff1b68
Sappo <rocktasion6@gmail.com>
2026-05-15 15:25:39 +0800

    Uno

--- lib/app/data/providers/film_provider.dart
+++ lib/app/data/providers/film_provider.dart
@@ -1,41 +1,43 @@
-import 'package:get/get.dart';
+import 'package:uno/uno.dart';
 import '../models/film_model.dart';
 
-class FilmProvider extends GetConnect {
-  // Base URL dari API publik Anda
+class FilmProvider {
+  // 1. Inisialisasi Uno
+  final uno = Uno();
+  
+  // Base URL tetap sama
   final String url = "https://68ff8dfbe02b16d1753e765d.mockapi.io/film";
 
-  @override
-  void onInit() {
-    // Konfigurasi GetConnect (opsional)
-    httpClient.timeout = const Duration(seconds: 20);
-    super.onInit();
-  }
+  // Pengganti onInit untuk timeout (Opsional di Uno)
+  // Uno secara default sangat stabil, tapi kita bisa atur manual jika perlu.
 
   // 1. READ (Ambil semua data film)
   Future<List<FilmModel>> getAllFilms() async {
-    final response = await get(url);
-    if (response.status.hasError) {
-      return Future.error(response.statusText ?? "Terjadi kesalahan");
-    } else {
-      // Mengubah List JSON menjadi List Object FilmModel
-      List data = response.body;
+    try {
+      final response = await uno.get(url);
+      
+      // Di Uno, data otomatis di-parse menjadi List/Map dan ada di properti .data
+      List data = response.data;
       return data.map((e) => FilmModel.fromJson(e)).toList();
+    } catch (e) {
+      // Menangani error seperti response.statusText
+      return Future.error("Terjadi kesalahan: $e");
     }
   }
 
   // 2. CREATE (Tambah data - Khusus Admin)
-  Future<Response> postFilm(FilmModel film) {
-    return post(url, film.toJson());
+  Future<Response> postFilm(FilmModel film) async {
+    // Uno mengembalikan objek Response yang strukturnya mirip GetConnect
+    return await uno.post(url, data: film.toJson());
   }
 
   // 3. UPDATE (Ubah data - Khusus Admin)
-  Future<Response> updateFilm(String id, FilmModel film) {
-    return put('$url/$id', film.toJson());
+  Future<Response> updateFilm(String id, FilmModel film) async {
+    return await uno.put('$url/$id', data: film.toJson());
   }
 
   // 4. DELETE (Hapus data - Khusus Admin)
-  Future<Response> deleteFilm(String id) {
-    return delete('$url/$id');
+  Future<Response> deleteFilm(String id) async {
+    return await uno.delete('$url/$id');
   }
 }
